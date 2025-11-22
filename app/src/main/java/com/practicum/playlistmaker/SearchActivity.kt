1a617e1100072276261e44e89277ff6e7dc17a7b
bezFantazee <davydovaksenia529@gmail.com>
2025-11-22 12:28:05 +0300

    -

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -37,8 +37,8 @@ class SearchActivity : AppCompatActivity() {
         .build()
 
     private val tracksService = retrofit.create(TrackApi::class.java)
-    private val tracks = ArrayList<Track>()
-    val tracksAdapter = TracksAdapter(tracks)
+    private val tracks = mutableListOf<Track>()
+    private val tracksAdapter = TracksAdapter(tracks)
 
     private lateinit var editText: EditText
     private lateinit var searchPlaceHolderImg: ImageView
@@ -84,27 +84,7 @@ class SearchActivity : AppCompatActivity() {
         backButton.setOnClickListener {
             finish()
         }
-//        val settingsButton = findViewById<LinearLayout>(R.id.settings_button)
-//
-//        settingsButton.setOnClickListener {
-//            val settingsIntent = Intent(this, SettingsActivity::class.java)
-//            startActivity(settingsIntent)
-//        }
 
-//        val mediaLibraryButton = findViewById<LinearLayout>(R.id.media_library_button)
-//
-//        mediaLibraryButton.setOnClickListener {
-//            val mediaLibraryIntent = Intent(this, MediaLibraryActivity::class.java)
-//            startActivity(mediaLibraryIntent)
-//        }
-
-
-        //логика отображения списка треков
-//        val tracks = listOf(Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-//            Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-//            Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-//            Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-//            Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"))
 
         editText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
@@ -167,18 +147,18 @@ class SearchActivity : AppCompatActivity() {
                     call: Call<TracksResponse?>,
                     response: Response<TracksResponse?>
                 ) {
-                    when(response.code()){
-                        200 -> {
-                            tracks.clear()
-                            if(response.body()?.results?.isNotEmpty() == true){
-                                tracks.addAll(response.body()?.results!!)
-                                tracksAdapter.notifyDataSetChanged()
-                                showPlaceholder("", "")//убираем заглушку
-                            } else {
-                                showPlaceholder(getString(R.string.empty_search).toString(), "") //показываем заглушку(ничего не найдено)
-                            }
+                    if(response.isSuccessful()) {
+                        tracks.clear()
+                        val body = response.body()?.results
+                        if (body?.isNotEmpty() == true) {
+                            tracks.addAll(body)
+                            tracksAdapter.notifyDataSetChanged()
+                            showPlaceholder("", "")//убираем заглушку
+                        } else {
+                            showPlaceholder(getString(R.string.empty_search).toString(), "") //показываем заглушку(ничего не найдено)
                         }
-                        else -> showPlaceholder(getString(R.string.connect_error).toString(), getString(R.string.extra_connect_error).toString())//показываем заглушку(ошибка подключения)
+                    } else {
+                        showPlaceholder(getString(R.string.connect_error).toString(), getString(R.string.extra_connect_error).toString())//показываем заглушку(ошибка подключения)
                     }
                 }
 

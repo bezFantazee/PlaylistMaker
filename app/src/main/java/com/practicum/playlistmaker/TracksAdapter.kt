537cc78563ed98ba930ed5d0a38e54ef4d57eb5a
bezFantazee <davydovaksenia529@gmail.com>
2025-12-03 01:23:49 +0300

    -

--- app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
@@ -1,8 +1,10 @@
 package com.practicum.playlistmaker
 
+import android.content.Context.MODE_PRIVATE
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.practicum.playlistmaker.SearchActivity.Companion.SEARCH_PREFERENCES
 
 class TracksAdapter(
     private val tracks: List<Track>
@@ -19,7 +21,12 @@ class TracksAdapter(
         holder: TracksViewHolder,
         position: Int
     ) {
+        val track = tracks[position]
         holder.bind(tracks[position])
+        holder.itemView.setOnClickListener {
+            val sharePref = holder.itemView.context.getSharedPreferences(SEARCH_PREFERENCES, MODE_PRIVATE)
+            SearchHistory(sharePref).saveTrack(track)
+        }
     }
 
     override fun getItemCount(): Int {
